3bfdebc54d7c89b959287e89ecbb2d0e307e1a7c
reschoene <reschoene@gmail.com>
2020-12-17 17:34:49 -0300

    first commit

---
+++ src/main/java/br/com/reschoene/SavePersonHandler.java
@@ -0,0 +1,47 @@
+package br.com.reschoene;
+
+import br.com.reschoene.model.PersonRequest;
+import br.com.reschoene.model.PersonResponse;
+import com.amazonaws.regions.Region;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.Item;
+import com.amazonaws.services.dynamodbv2.document.PutItemOutcome;
+import com.amazonaws.services.dynamodbv2.document.spec.PutItemSpec;
+import com.amazonaws.services.dynamodbv2.model.ConditionalCheckFailedException;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+public class SavePersonHandler implements RequestHandler<PersonRequest, PersonResponse> {
+    private DynamoDB dynamoDb;
+    private String DYNAMODB_TABLE_NAME = "Person";
+    private Regions REGION = Regions.SA_EAST_1; //South America - Sao Paulo
+
+    @Override
+    public PersonResponse handleRequest(PersonRequest personRequest, Context context) {
+        this.initDynamoDbClient();
+
+        persistData(personRequest);
+
+        PersonResponse personResponse = new PersonResponse();
+        personResponse.setMessage("Saved Successfully!!!");
+        return personResponse;
+    }
+
+    private PutItemOutcome persistData(PersonRequest personRequest)
+            throws ConditionalCheckFailedException {
+        return this.dynamoDb.getTable(DYNAMODB_TABLE_NAME)
+                .putItem(
+                        new PutItemSpec().withItem(new Item()
+                                .withInt("id", personRequest.getId())
+                                .withString("firstName", personRequest.getFirstName())
+                                .withString("lastName", personRequest.getLastName())));
+    }
+
+    private void initDynamoDbClient() {
+        AmazonDynamoDBClient client = new AmazonDynamoDBClient();
+        client.setRegion(Region.getRegion(REGION));
+        this.dynamoDb = new DynamoDB(client);
+    }
+}
---
+++ src/main/java/br/com/reschoene/model/PersonResponse.java
@@ -0,0 +1,13 @@
+package br.com.reschoene.model;
+
+public class PersonResponse {
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
